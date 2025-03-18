93e0c3a72bdbbe1bd3271e1b8fbcab76286ee1da
Numimimi <wave2547@gmail.com>
2025-03-19 03:09:08 +0700

    merge user

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
-const bodyParser = require('body-parser');
 
 // Load environment variables
 dotenv.config();
@@ -17,7 +16,6 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(express.json()); // Parse JSON requests
 app.use(cors()); // Enable CORS
-app.use(bodyParser.json());
 
 // Sample route
 app.get("/", (req, res) => {
@@ -25,20 +23,9 @@ app.get("/", (req, res) => {
 });
 app.use('/api/users', userRoutes);
 
-
 // Sample API query
-async function getBlogs() {
-  try {
-    let { data: users, error } = await supabase
-    .from('users')
-    .select('*')
-    return users
-  } catch (error) {
-    console.log(error)
-  }
-}
 
-getBlogs().then((blogs) => {console.log(blogs)})
+
 
 // Start the server
 app.listen(PORT, () => {
